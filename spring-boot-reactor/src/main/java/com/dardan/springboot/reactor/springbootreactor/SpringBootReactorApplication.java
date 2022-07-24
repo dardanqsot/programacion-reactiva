86abaeb589b3ff5e4668d4897ff7145d1d81d1b0
darwinQSot <quispesotodaniel@gmail.com>
2022-07-24 18:44:24 -0500

    S2: Reactor API - Programación reactiva desde la base: El operador map.

--- spring-boot-reactor/src/main/java/com/dardan/springboot/reactor/springbootreactor/SpringBootReactorApplication.java
+++ spring-boot-reactor/src/main/java/com/dardan/springboot/reactor/springbootreactor/SpringBootReactorApplication.java
@@ -1,5 +1,6 @@
 package com.dardan.springboot.reactor.springbootreactor;
 
+import com.dardan.springboot.reactor.springbootreactor.model.Usuario;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -17,15 +18,21 @@ public class SpringBootReactorApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        Flux<String> nombres = Flux.just("Darwin", "Pedro", "Daniel", "Diego", "Pepe")
-                .doOnNext(e -> {
-                    if(e.isEmpty()){
+        Flux<Usuario> nombres = Flux.just("Darwin", "Pedro", "Daniel", "Diego", "Pepe")
+                .map(nombre -> new Usuario(nombre.toUpperCase(), null))
+                .doOnNext(usuario -> {
+                    if(usuario == null){
                         throw new RuntimeException("Nombres no pueden ser vacíos");
                     }
-                    System.out.println(e);
+                    System.out.println(usuario);
+                })
+                .map(usuario -> {
+                    String nombre = usuario.getNombre().toLowerCase();
+                    usuario.setNombre(nombre);
+                    return usuario;
                 });
 
-        nombres.subscribe(e -> log.info(e),
+        nombres.subscribe(e -> log.info(e.toString()),
                 error -> log.error(error.getMessage()),
                 new Runnable() {
                     @Override
