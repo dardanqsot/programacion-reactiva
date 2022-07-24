4be8f06c88e516b0d9f68e44819d626dff98f154
darwinQSot <quispesotodaniel@gmail.com>
2022-07-24 18:32:28 -0500

    S2: Reactor API - Programación reactiva desde la base: El método subscribe().

--- spring-boot-reactor/src/main/java/com/dardan/springboot/reactor/springbootreactor/SpringBootReactorApplication.java
+++ spring-boot-reactor/src/main/java/com/dardan/springboot/reactor/springbootreactor/SpringBootReactorApplication.java
@@ -1,6 +1,7 @@
 package com.dardan.springboot.reactor.springbootreactor;
 
-import ch.qos.logback.core.net.SyslogOutputStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,15 +10,22 @@ import reactor.core.publisher.Flux;
 @SpringBootApplication
 public class SpringBootReactorApplication implements CommandLineRunner {
 
+    private static final Logger log = LoggerFactory.getLogger(SpringBootReactorApplication.class);
     public static void main(String[] args) {
         SpringApplication.run(SpringBootReactorApplication.class, args);
     }
 
     @Override
     public void run(String... args) throws Exception {
-        Flux<String> nombres = Flux.just("Darwin", "Pedro", "Diego", "Pepe")
-                .doOnNext(System.out::println);
+        Flux<String> nombres = Flux.just("Darwin", "Pedro", "", "Diego", "Pepe")
+                .doOnNext(e -> {
+                    if(e.isEmpty()){
+                        throw new RuntimeException("Nombres no pueden ser vacíos");
+                    }
+                    System.out.println(e);
+                });
 
-        nombres.subscribe();
+        nombres.subscribe(e -> log.info(e),
+                error -> log.error(error.getMessage()));
     }
 }
