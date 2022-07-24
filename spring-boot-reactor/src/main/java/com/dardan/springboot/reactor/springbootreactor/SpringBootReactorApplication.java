f94030647c16b47f3acd91f3d9bd64e04182db19
darwinQSot <quispesotodaniel@gmail.com>
2022-07-24 18:25:22 -0500

    S2: Reactor API - Programación reactiva desde la base: Creando un stream Flux Observable.

--- spring-boot-reactor/src/main/java/com/dardan/springboot/reactor/springbootreactor/SpringBootReactorApplication.java
+++ spring-boot-reactor/src/main/java/com/dardan/springboot/reactor/springbootreactor/SpringBootReactorApplication.java
@@ -1,13 +1,23 @@
 package com.dardan.springboot.reactor.springbootreactor;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Flux;
 
 @SpringBootApplication
-public class SpringBootReactorApplication {
+public class SpringBootReactorApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootReactorApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        Flux<String> nombres = Flux.just("Darwin", "Pedro", "Diego", "Pepe")
+                .doOnNext(System.out::println);
+
+        nombres.subscribe();
+    }
 }
