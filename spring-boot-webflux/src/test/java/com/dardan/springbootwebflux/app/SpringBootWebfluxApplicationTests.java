fcb68dcffb5bcb0775a396ce54bfd79ad1ad5ba3
darwinQSot <quispesotodaniel@gmail.com>
2022-07-29 10:47:58 -0500

    S3: Introducción Spring WebFlux - Creando proyecto con Spring webflux

---
+++ spring-boot-webflux/src/test/java/com/dardan/springbootwebflux/app/SpringBootWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dardan.springbootwebflux.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebfluxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
