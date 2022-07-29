17e8a67aaba260662df6c05aebb1f736434ff49d
darwinQSot <quispesotodaniel@gmail.com>
2022-07-29 06:56:27 -0500

    S2: Reactor API - Programación reactiva desde la base: El operador range

--- spring-boot-reactor/src/main/java/com/dardan/springboot/reactor/springbootreactor/SpringBootReactorApplication.java
+++ spring-boot-reactor/src/main/java/com/dardan/springboot/reactor/springbootreactor/SpringBootReactorApplication.java
@@ -24,7 +24,14 @@ public class SpringBootReactorApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        ejemploUsuarioComentariosZipWith();
+        ejemploZipWithRangos();
+    }
+
+    public void ejemploZipWithRangos() {
+        Flux<Integer> rangos = Flux.range(0, 4);
+        Flux.just(1, 2, 3, 4).map(i -> (i * 2))
+                .zipWith(rangos, (uno, dos) -> String.format("Primer Flux: %d, Segundo Flux: %d", uno, dos))
+                .subscribe(texto -> log.info(texto));
     }
 
     public void ejemploUsuarioComentariosZipWith() {
