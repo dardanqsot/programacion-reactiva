4181196325aef74c345a54f876cf4e08a505acbc
darwinQSot <quispesotodaniel@gmail.com>
2022-07-29 11:22:24 -0500

    S3: Introducción Spring WebFlux - Eliminando coleccion de productos (drop collection)

--- spring-boot-webflux/src/main/java/com/dardan/springbootwebflux/app/SpringBootWebfluxApplication.java
+++ spring-boot-webflux/src/main/java/com/dardan/springbootwebflux/app/SpringBootWebfluxApplication.java
@@ -1,5 +1,7 @@
 package com.dardan.springbootwebflux.app;
 
+import java.util.Date;
+
 import com.dardan.springbootwebflux.app.models.dao.ProductoDao;
 import com.dardan.springbootwebflux.app.models.documents.Producto;
 import org.slf4j.Logger;
@@ -8,19 +10,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
+
 import reactor.core.publisher.Flux;
 
-import java.util.Date;
-
 @SpringBootApplication
-public class SpringBootWebfluxApplication implements CommandLineRunner {
+public class SpringBootWebfluxApplication implements CommandLineRunner{
 
     @Autowired
     private ProductoDao dao;
 
+    @Autowired
+    private ReactiveMongoTemplate mongoTemplate;
 
     private static final Logger log = LoggerFactory.getLogger(SpringBootWebfluxApplication.class);
-
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootWebfluxApplication.class, args);
@@ -28,6 +31,8 @@ public class SpringBootWebfluxApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+
+        mongoTemplate.dropCollection("productos").subscribe();
 
         Flux.just(new Producto("TV Panasonic Pantalla LCD", 456.89),
                         new Producto("Sony Camara HD Digital", 177.89),
